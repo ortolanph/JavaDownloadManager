54c635959e6688e14cca86fe680dc1d7958cf62f
ortolanph <des07pho>
2017-02-10 23:28:21 -0200

    Refatorando projeto

--- src/main/java/org/jdm/controller/DownloadManager.java
+++ src/main/java/org/jdm/controller/DownloadManager.java
@@ -9,20 +9,95 @@ import org.jdm.ui.Centralizer;
 import org.jdm.ui.DownloadManagerWindow;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class DownloadManager extends DownloadManagerWindow implements ActionListener {
+public class DownloadManager extends DownloadManagerWindow {
 
     private static final long serialVersionUID = 1L;
 
     public DownloadManager() {
         setSize(800, 600);
         setLocation(Centralizer.getCenterPosition(getWidth(), getHeight()));
-        addActionListener(this);
-        refreshTable(new ArrayList<DownloadBean>());
+        addActionListener(event -> {
+            switch (event.getActionCommand()) {
+                case "START":
+                    removeCompleted();
+                    List<DownloadBean> downloads = getDownloadList();
+
+                    DownloadProcess dp = new DownloadProcess(this, downloads);
+                    Thread t = new Thread(dp);
+                    t.start();
+
+                    completeDownloadProcess();
+                    break;
+                case "NEW":
+                    DownloadAdder adder = new DownloadAdder(this);
+                    int addResult = adder.showDialog();
+
+                    if (addResult == DownloadAdder.ACTION_ACCEPTED) {
+                        List<DownloadBean> data = getDownloadList();
+                        data.add(adder.getBean());
+                        refreshTable(DownloadSorter.sortByStatus(data));
+                    }
+                    break;
+                case "DEL":
+                    List<DownloadBean> data = getDownloadList();
+                    List<DownloadBean> copy = new ArrayList<>();
+
+                    int[] selected = getSelectedDownloads();
+
+                    for (int i : selected) {
+                        data.set(i, null);
+                    }
+
+                    for (DownloadBean bean : data) {
+                        if (bean != null) {
+                            copy.add(bean);
+                        }
+                    }
+
+                    refreshTable(DownloadSorter.sortByStatus(copy));
+                    break;
+                case "COMP":
+                    removeCompleted();
+                    break;
+                case "SAVE":
+                    JFileChooser saveDialog = new JFileChooser();
+
+                    int saveResult = saveDialog.showSaveDialog(this);
+
+                    if (saveResult == JFileChooser.APPROVE_OPTION) {
+                        DownloadSaver.save(saveDialog.getSelectedFile(), getDownloadList());
+                    }
+                    break;
+                case "LOAD":
+                    List<DownloadBean> loadData = new ArrayList<>();
+
+                    JFileChooser openDialog = new JFileChooser();
+
+                    int result = openDialog.showOpenDialog(this);
+
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        loadData = DownloadSaver.load(openDialog.getSelectedFile());
+                    }
+
+                    if (loadData != null) {
+                        refreshTable(DownloadSorter.sortByStatus(loadData));
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Invalid file or corrupted.", "Error on loading file", JOptionPane.ERROR_MESSAGE);
+                    }
+                    break;
+                case "EXIT":
+                    System.exit(0);
+                    break;
+                default:
+
+            }
+        });
+
+        refreshTable(new ArrayList<>());
     }
 
     public static void main(String[] args) {
@@ -30,110 +105,26 @@ public class DownloadManager extends DownloadManagerWindow implements ActionList
         manager.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent event) {
-        String action = event.getActionCommand();
-
-        if (action.equals("START")) {
-            removeCompleted();
-            List<DownloadBean> downloads = getDownloadList();
-
-            DownloadProcess dp = new DownloadProcess(this, downloads);
-            Thread t = new Thread(dp);
-            t.start();
-
-            completeDownloadProcess();
-        }
-
-        if (action.equals("NEW")) {
-            DownloadAdder adder = new DownloadAdder(this);
-            int result = adder.showDialog();
-
-            if (result == DownloadAdder.ACTION_ACCEPTED) {
-                List<DownloadBean> data = getDownloadList();
-                data.add(adder.getBean());
-                refreshTable(DownloadSorter.sortByStatus(data));
-            }
-        }
-
-        if (action.equals("DEL")) {
-            List<DownloadBean> data = getDownloadList();
-            List<DownloadBean> copy = new ArrayList<>();
-
-            int[] selected = getSelectedDownloads();
-
-            for (int i : selected) {
-                data.set(i, null);
-            }
-
-            for (DownloadBean bean : data) {
-                if (bean != null) {
-                    copy.add(bean);
-                }
-            }
-
-            refreshTable(DownloadSorter.sortByStatus(copy));
-        }
-
-        if (action.equals("COMP")) {
-            removeCompleted();
-        }
-
-        if (action.equals("SAVE")) {
-            JFileChooser saveDialog = new JFileChooser();
-
-            int result = saveDialog.showSaveDialog(this);
-
-            if (result == JFileChooser.APPROVE_OPTION) {
-                DownloadSaver.save(saveDialog.getSelectedFile(), getDownloadList());
-            }
-        }
-
-        if (action.equals("LOAD")) {
-            List<DownloadBean> data = new ArrayList<>();
-
-            JFileChooser openDialog = new JFileChooser();
-
-            int result = openDialog.showOpenDialog(this);
-
-            if (result == JFileChooser.APPROVE_OPTION) {
-                data = DownloadSaver.load(openDialog.getSelectedFile());
-            }
-
-            if (data != null) {
-                refreshTable(DownloadSorter.sortByStatus(data));
-            } else {
-                JOptionPane.showMessageDialog(this, "Invalid file or corrupted.", "Error on loading file", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-
-        if (action.equals("EXIT")) {
-            System.exit(0);
-        }
-    }
-
     public void updateTable(DownloadBean bean) {
         List<DownloadBean> data = getDownloadList();
-
         data.set(data.indexOf(bean), bean);
-
         refreshTable(data);
     }
 
     protected void removeCompleted() {
         List<DownloadBean> data = getDownloadList();
-        List<DownloadBean> copy =  new ArrayList<>(getDownloadList());
+        List<DownloadBean> completedDownloads =
+                data
+                        .stream()
+                        .filter(d -> d.getStatus().equals(DownloadStatus.COMPLETED))
+                        .collect(Collectors.toList());
 
-        for (DownloadBean bean : data) {
-            if (bean.getStatus().equals(DownloadStatus.COMPLETED)) {
-                copy.remove(bean);
-            }
-        }
+        data.removeAll(completedDownloads);
 
-        refreshTable(DownloadSorter.sortByStatus(copy));
+        refreshTable(DownloadSorter.sortByStatus(data));
     }
 
     public void completeDownloadProcess() {
-        List<DownloadBean> data = getDownloadList();
-        refreshTable(DownloadSorter.sortByStatus(data));
+        refreshTable(DownloadSorter.sortByStatus(getDownloadList()));
     }
 }
